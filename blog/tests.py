88ceb08b607711544e1cbdfe221e6fbea0e007d9
ibrivasortiz92 <78561871+ibrivasortiz92@users.noreply.github.com>
2021-05-18 09:34:12 -0300

    commit test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+  def setUp(self):
+    self.blog = Post.objects.create(title = "Post 1", author = "User 1", slug = "Slug 1")
+
+  def test_db(self):
+    d = self.blog
+    self.assertTrue(isinstance(d, Post))
+    self.assertEqual(str(d), "Post 1")
